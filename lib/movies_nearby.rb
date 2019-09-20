9e64b9c3d940ae16e3a0027993f48ae173a87760
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-09-20 18:38:33 +0000

    refactored cli, console &  dependencies

--- lib/movies_nearby.rb
+++ lib/movies_nearby.rb
@@ -1,10 +1,10 @@
 require 'open-uri'
-require 'nokogiri'
-require 'pry'
+#require 'nokogiri'
+#require 'pry'
 
-require_relative "./movies_nearby/version"
-require_relative "./movies_nearby/movie"
-require_relative "./movies_nearby/cli"
+require_relative "../lib/movies_nearby/version"
+require_relative "../lib/movies_nearby/movies"
+require_relative "../lib/movies_nearby/cli"
 
 module MoviesNearby
   class Error < StandardError; end
