d6d4e7bdc4052253fb08e43112c71ab5f8bcb646
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-09-22 10:23:21 +0000

    created method in movies.rb to scrape IMDB website

--- lib/movies_nearby/movies.rb
+++ lib/movies_nearby/movies.rb
@@ -5,5 +5,27 @@ class MoviesNearby::Movies
     @name = name
     @url = url
   end
-
+  
+  def self.all 
+    @@all ||= scrape_movies_nearby
+  end 
+  
+  def self.scrape_movies 
+    movies = []
+    
+    movies << self.scrape_fandango
+  end 
+  
+  def self.scrape_fandango
+    doc = Nokogiri::HTML(open("https://fandango.com/movies-in-theatres/"))
+    
+    movie = self.new 
+    
+    movie.name =doc.search("span.heading-style-1 browse-movielist--title poster-card--title").text.strip
+    movie.url = doc.search('https://www.fandango.com/abominable-2019-218991/movie-overview').attr("href").value 
+    movie.summary = doc.search("p.mop__synopsis-content").text.strip
+    movie.stars = doc.search("div.carousel-cast-crew").collect{|e| e.text.strip}.join(",")
+    
+    movie 
+  end 
 end 
