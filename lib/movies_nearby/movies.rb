4e7af8c5d325c2ee5562ff938b98a4393ce6ae9b
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-10-02 15:38:05 +0000

    revised class MoviesNearby::Movies in movies.rb file to simplify CLI scraper logic.

--- lib/movies_nearby/movies.rb
+++ lib/movies_nearby/movies.rb
@@ -1,49 +1,31 @@
 class MoviesNearby::Movies 
-  attr_accessor :name, :url, :summary, :stars
+  attr_accessor :name, :location, :open, :url
 
-  def initialize(name = nil, url = nil)
-    @name = name
-    @url = url 
+  def self.nearby 
+    # Scrape fandango and return theaters nearby based on that data 
+    self.scrape_movies
   end
   
-  def self.all 
-    @@all ||= scrape_fandango
+  def self.scrape_movies 
+    movies = []
+    
+    movies << self.scrape_fandango
+    
+    movies 
   end 
   
-  def self.find(id)
-    self.all[id-1]
-  end 
-  
-  def self.find_by_name(name) 
-    self.all.detect do |m| 
-      m.name.downcase.strip == name.downcase.strip ||
-      m.name.split("(").first.strip.downcase == name.downcase.strip
-    end 
-  end 
-  
-  def summary
-    @summary ||= plot_summary_doc.search("p.mop__synopsis-content").text.strip
-  end
-
-  def stars
-    @stars ||= doc.search("dl.cast-and-crew__credits[itemprop='name']").collect{|e| e.text.strip}.join(", ")
-  end
-  
-  def plot_summary_doc
-      @plot_summary_doc ||= Nokogiri::HTML(open("#{self.url}plot-summary"))
-    end
-
   
   def self.scrape_fandango
-    doc = Nokogiri::HTML(open("https://www.fandango.com/movies-in-thearters"))
+    doc = Nokogiri::HTML(open("https://fandango.com/19063_movietimes"))
     
     binding.pry 
     
-    names = doc.search("span[itemprop='heading-style-1'] a[itemprop='url']")
-    names.collect{|e| new(e.text.strip, "https://www.fandango.com#{e.attr("href").split("?").first.strip}")}
-  end
-  
-  def doc
-      @doc ||= Nokogiri::HTML(open(self.url))
+    movie = self.new 
+    movie.name = doc.search("h3.fd-theater__name").text.strip
+    movie.location = doc.search("div.fd-theater__address-wrap")
+    movie.open = true 
+    movie.url = doc.search("a.light").attr("href").strip
+    
+    movie 
   end
 end 
