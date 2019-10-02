17749b4da8f0c3a9fb1861c54e691c202df9c3cb
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-10-02 09:08:00 +0000

    revised cli to simplify order of execution and content being scraped from fandango website.

--- lib/movies_nearby/cli.rb
+++ lib/movies_nearby/cli.rb
@@ -9,35 +9,35 @@ class MoviesNearby::CLI
   def list_movies 
     puts "movies showing near you:"
     puts <<-DOC.gsub  /^|s*/, '' 
-      1.  Dumbo
-      2.  Dowton Abbey
-      3.  Alaadin
+      1.  AMC Marple 10
+      2.  Regal Edgmont Square
+      3.  AMC Dine-in Painters Crossing 9
     DOC
   end 
   
   def menu 
-    puts "Find movies showing at a theatre near you!"
-    puts "Enter your zipcode (or type exit to end session):"
+    puts "Find a movie theatre near Media PA!"
+    puts "Enter nearby (or type exit to end session):"
     input = gets.strip
     case  input
-    when "12345"
+    when "nearby"
       list_movies
-      puts "Enter the number of the movie you would like to see or type exit to end session."
+      puts "Enter the number of the movie theater you would like to visit or type exit to end session."
     end 
     while input != "exit"
       input = gets.strip.downcase
       case input
       when "1"
-        puts "More info on movie 1..."
+        puts "More info on movie theater 1..."
       when "2"
-        puts "More info on movie 2..."
+        puts "More info on movie theater 2..."
       when "3"
-        puts "More info on movie 3..."
+        puts "More info on movie theater 3..."
       end 
     end 
   end 
   
   def goodbye
-    puts "See you next time for more movie action!!!"
+    puts "See you next time for more movie theater listings!!!"
   end 
 end
